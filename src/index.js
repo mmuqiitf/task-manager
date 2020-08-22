a6f6e488d2a884eebadc7715d8fe28dd1cf45ca9
M. Muqiit Faturrahman <mmuqiit.f14@gmail.com>
2020-08-22 22:29:26 +0700

    upload-files

--- src/index.js
+++ src/index.js
@@ -13,13 +13,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const jwt = require('jsonwebtoken')
-const myF = async () => {
-    // const token = jwt.sign({ _id: 'avc123' }, 'XkeGwksQoPwzV1JDFs6JMZBmNU7DrD3Zg6', {expiresIn: '7 days'})
-    // console.log(token)
-    // const data = jwt.verify(token, 'XkeGwksQoPwzV1JDFs6JMZBmNU7DrD3Zg6')
-    // console.log(data)
-}
-
-myF()
